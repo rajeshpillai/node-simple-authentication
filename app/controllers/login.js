42103e168aca854657568a386ee27e704f5766ae
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-08 11:05:24 +0530

    Added the authenticate method in login controller

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  loginName: null,
+  password: null,
+  session: Ember.inject.service(),
+  actions: {
+    authenticate() {
+      this.get("session")
+        .authenticate(this.get("loginName"),
+          this.get("password"))
+        .then(()=> {
+          alert("Logged in!");
+          this.transitionToRoute("students");
+        }, (err) => {
+          alert("Error with login! " + err.responseText);
+        });
+    }
+  }
 });
